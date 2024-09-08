f31507ac1859e24c7c50f742b9663acfdfae854a
Alf <42708476+alfset@users.noreply.github.com>
2024-09-08 17:28:39 +0700

    Update CoFinanceFactory.sol

--- DEFI/CoFinance/contract/CoFinanceFactory.sol
+++ DEFI/CoFinance/contract/CoFinanceFactory.sol
@@ -5,17 +5,18 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./CoFinance.sol";
 import "./LiquidityToken.sol";
 import "./Staking.sol";
+import "./CollateralLockPool.sol";
 
 contract CoFinanceFactory {
-    uint256 public creationFee  = 10;
-    uint public Reward = 10;
+    uint256 public creationFee = 10;
     address public owner;
-    uint256 public maxFeePercent; // Maximum fee percentage allowed for pools
-    address[] public allPools; 
+    address public immutable thisAddress; 
+    address[] public allPools;
     mapping(address => mapping(address => address)) public pools; 
     mapping(address => address[]) public poolsByToken;
-    mapping(address => address) public poolByPair; 
     mapping(address => bool) public incentivizedPools;
+
+    mapping(address => address) public collateralLockPools;
 
     event PoolCreated(
         address indexed poolAddress,
@@ -25,20 +26,23 @@ contract CoFinanceFactory {
         address rewardToken,
         address priceFeed,
         address stakingContract,
-        bool isPoolIncentivized,
-        address factory
+        address collateralLockPool, 
+        bool isPoolIncentivized
     );
     event CreationFeeUpdated(uint256 newFee);
     event FeesWithdrawn(address indexed owner, address token, uint256 amount);
-    event PoolIncentivized(address indexed poolAddress); // Declared event
+    event PoolIncentivized(address indexed poolAddress);
 
     constructor() {
         owner = msg.sender;
+        thisAddress = address(this);  
     }
+    
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this function");
         _;
     }
+
     function createPool(
         address tokenA,
         address tokenB,
@@ -47,19 +51,21 @@ contract CoFinanceFactory {
         string memory liquidityTokenName,
         string memory liquidityTokenSymbol,
         bool isPoolIncentivized
-
     ) external payable returns (address) {
         require(msg.value == creationFee, "Incorrect ETH amount sent");
         require(tokenA != tokenB, "Token addresses must be different");
-        if (tokenA > tokenB) {           
+
+        if (tokenA > tokenB) {
             (tokenA, tokenB) = (tokenB, tokenA);
         }
+
         address existingPool = pools[tokenA][tokenB];
         if (existingPool != address(0)) {
             return existingPool; 
         }
+        CollateralLockPool collateralLockPool = new CollateralLockPool(tokenA, tokenB);
         LiquidityToken liquidityToken = new LiquidityToken(liquidityTokenName, liquidityTokenSymbol);
-        Staking stakingContract = new Staking(address(liquidityToken), rewardToken); 
+        Staking stakingContract = new Staking(address(liquidityToken), rewardToken);
         CoFinance pool = new CoFinance(
             tokenA,
             tokenB,
@@ -67,17 +73,22 @@ contract CoFinanceFactory {
             priceFeed,
             address(liquidityToken),
             address(stakingContract),
+            address(collateralLockPool), 
             isPoolIncentivized,
-            address(this)
+            thisAddress
         );
         liquidityToken.setCoFinanceContract(address(pool));
-        pools[tokenA][tokenB] = address(pool);
+        pools[tokenA][tokenB] =address(pool);
         poolsByToken[tokenA].push(address(pool));
         poolsByToken[tokenB].push(address(pool));
         allPools.push(address(pool));
+
         if (isPoolIncentivized) {
             incentivizedPools[address(pool)] = true;
         }
+        
+        collateralLockPools[address(pool)] = address(collateralLockPool);
+
         emit PoolCreated(
             address(pool),
             tokenA,
@@ -86,65 +97,72 @@ contract CoFinanceFactory {
             rewardToken,
             priceFeed,
             address(stakingContract),
-            isPoolIncentivized,
-            address(this)
+            address(collateralLockPool),
+            isPoolIncentivized
         );
         return address(pool);
     }
+
     function getAllPools() external view returns (address[] memory) {
         return allPools;
     }
+
     function updateCreationFee(uint256 newFee) external onlyOwner {
         creationFee = newFee;
         emit CreationFeeUpdated(newFee);
     }
+
     function getPoolByToken(address token) external view returns (address[] memory) {
         return poolsByToken[token];
     }
+
     function getPoolByPair(address tokenA, address tokenB) external view returns (address) {
-        if (tokenA > tokenB) {
+        if (tokenA != tokenB) {
             (tokenA, tokenB) = (tokenB, tokenA);
         }
         return pools[tokenA][tokenB];
     }
+
     function withdrawFees(address tokenAddress) external onlyOwner {
         if (tokenAddress == address(0)) {
-            uint256 balance = address(this).balance;
+            uint256 balance = thisAddress.balance;  // Use cached address
             require(balance > 0, "No ETH to withdraw");
-            payable(owner).transfer(balance);
+            // Use call instead of transfer
+            (bool success, ) = owner.call{value: balance}("");
+            require(success, "Transfer failed");
             emit FeesWithdrawn(owner, address(0), balance);
         } else {
             IERC20 token = IERC20(tokenAddress);
-            uint256 balance = token.balanceOf(address(this));
+            uint256 balance = token.balanceOf(thisAddress); 
             require(balance > 0, "No ERC20 tokens to withdraw");
-            token.transfer(owner, balance);
+            // Use call instead of transfer
+            bool success = token.transfer(owner, balance);
+            require(success, "Transfer failed");
             emit FeesWithdrawn(owner, tokenAddress, balance);
         }
     }
 
     function getIncentivizedPools() external view returns (address[] memory) {
-        address[] memory incentivizedPoolList = new address[](allPools.length);
+        uint256 length = allPools.length;
         uint256 count = 0;
-
-        for (uint256 i = 0; i < allPools.length; i++) {
+        
+        for (uint256 i = 0; i < length; ++i) {
             if (incentivizedPools[allPools[i]]) {
-                incentivizedPoolList[count] = allPools[i];
                 count++;
             }
         }
+        
         address[] memory result = new address[](count);
-        for (uint256 j = 0; j < count; j++) {
-            result[j] = incentivizedPoolList[j];
+        uint256 index = 0;
+        for (uint256 i = 0; i < length; ++i) {
+            if (incentivizedPools[allPools[i]]) {
+                result[index++] = allPools[i];
+            }
         }
         return result;
     }
 
-    function updateIncentivizedPool(address poolAddress, address tokenA, address tokenB) external {
-        if (tokenA > tokenB) {
-            (tokenA, tokenB) = (tokenB, tokenA);
-        }
-        require(pools[tokenA][tokenB] == poolAddress, "Pool not found");
-        incentivizedPools[poolAddress] = true;
-        emit PoolIncentivized(poolAddress);
+    function getCollateralLockPool(address poolAddress) external view returns (address) {
+        return collateralLockPools[poolAddress];
     }
 }
