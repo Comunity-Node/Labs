b95e77cd12895eaf65cb97d3a4698f0e8ab591bd
Alf <42708476+alfset@users.noreply.github.com>
2024-09-09 10:12:48 +0700

    Update Staking.sol

--- DEFI/CoFinance/contract/Staking.sol
+++ DEFI/CoFinance/contract/Staking.sol
@@ -1,23 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./Interface/ILiquidityToken.sol";
+import "./Interface/ICoFinance.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-interface ICoFinance {
-    function calculateInterest(uint256 amount) external view returns (uint256);
-    function SWAP_FEE_PERCENT() external view returns (uint256);
-    function APR_7_DAYS() external view returns (uint256);
-    function APR_14_DAYS() external view returns (uint256);
-    function APR_21_DAYS() external view returns (uint256);
-    function SECONDS_IN_7_DAYS() external view returns (uint256);
-    function SECONDS_IN_14_DAYS() external view returns (uint256);
-    function SECONDS_IN_21_DAYS() external view returns (uint256);
-    function rewardToken() external view returns (address);
-}
 
-contract Staking {
-    using SafeMath for uint256;
+contract Staking is ReentrancyGuard  {
+
     uint256 public immutable APR_7_DAYS = 20; // 20% APR for 7 days
     uint256 public immutable APR_14_DAYS = 30; // 30% APR for 14 days
     uint256 public immutable APR_21_DAYS = 50; // 50% APR for 21 days
@@ -25,40 +16,37 @@ contract Staking {
     uint256 public constant SECONDS_IN_14_DAYS = 14 days;
     uint256 public constant SECONDS_IN_21_DAYS = 21 days;
 
-    IERC20 public liquidityToken;
+    ILiquidityToken public liquidityToken;
     ICoFinance public coFinance;
-    bool public isPoolIncentivized;
 
     mapping(address => uint256) public stakerBalances;
     mapping(address => uint256) public stakingStartTimes;
     mapping(address => uint256) public rewardBalances;
 
     constructor(address _liquidityToken, address _coFinance) {
-        liquidityToken = IERC20(_liquidityToken);
+        liquidityToken = ILiquidityToken(_liquidityToken);
         coFinance = ICoFinance(_coFinance);
     }
 
     function stake(uint256 amount) external {
         require(amount > 0, "Amount must be greater than 0");
-        liquidityToken.transferFrom(msg.sender, address(this), amount);
+        IERC20(address(liquidityToken)).transferFrom(msg.sender, address(this), amount);
         updateRewards(msg.sender);
-        stakerBalances[msg.sender] = stakerBalances[msg.sender].add(amount);
+        stakerBalances[msg.sender] = stakerBalances[msg.sender] + amount;
         stakingStartTimes[msg.sender] = block.timestamp;
-        updateIncentives();
     }
 
     function withdraw(uint256 amount) external {
         require(stakerBalances[msg.sender] >= amount, "Insufficient balance");
         updateRewards(msg.sender);
-        stakerBalances[msg.sender] = stakerBalances[msg.sender].sub(amount);
-        liquidityToken.transfer(msg.sender, amount);
-        updateIncentives();
+        stakerBalances[msg.sender] = stakerBalances[msg.sender] - (amount);
+        IERC20(address(liquidityToken)).transfer(msg.sender, amount);
     }
 
     function claimReward() external {
         updateRewards(msg.sender);
         uint256 reward = rewardBalances[msg.sender];
-        require(reward > 0, "No rewards to claim");
+        require(reward != 0, "No rewards to claim");
         rewardBalances[msg.sender] = 0;
         IERC20 rewardToken = IERC20(coFinance.rewardToken());
         rewardToken.transfer(msg.sender, reward);
@@ -67,19 +55,15 @@ contract Staking {
     function updateRewards(address staker) internal {
         uint256 stakerBalance = stakerBalances[staker];
         uint256 stakingDuration = block.timestamp - stakingStartTimes[staker];
-        
-        uint256 apr = getAPRForDuration(stakingDuration);
-        uint256 newReward = stakerBalance
-            .mul(apr)
-            .div(100)
-            .mul(stakingDuration)
-            .div(365 days);
 
-        rewardBalances[staker] = rewardBalances[staker].add(newReward);
+        uint256 apr = getAPRForDuration(stakingDuration);
+        uint256 newReward = stakerBalance * apr / stakingDuration / 100 / 365 days;
+
+        rewardBalances[staker] = rewardBalances[staker] + newReward;
         stakingStartTimes[staker] = block.timestamp;
     }
 
-    function getAPRForDuration(uint256 duration) internal view returns (uint256) {
+    function getAPRForDuration(uint256 duration) internal pure returns (uint256) {
         if (duration <= SECONDS_IN_7_DAYS) {
             return APR_7_DAYS;
         } else if (duration <= SECONDS_IN_14_DAYS) {
@@ -89,9 +73,5 @@ contract Staking {
         } else {
             return 0;
         }
-    }
-
-    function updateIncentives() internal {
-        isPoolIncentivized = liquidityToken.balanceOf(address(this)) > 0;
     }
 }
