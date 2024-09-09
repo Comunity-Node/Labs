e61e8977fbe8aabb1f973bb7850f469bddf807a4
Alf <42708476+alfset@users.noreply.github.com>
2024-09-09 09:57:39 +0700

    Update CollateralLockPool.sol

--- DEFI/CoFinance/contract/CollateralLockPool.sol
+++ DEFI/CoFinance/contract/CollateralLockPool.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract CollateralLockPool {
+
+contract CollateralLockPool is ReentrancyGuard {
     using SafeMath for uint256;
 
     IERC20 public tokenA;
@@ -27,25 +29,24 @@ contract CollateralLockPool {
         
         if (tokenAddress == address(tokenA)) {
             tokenA.transferFrom(user, address(this), amount);
-            lockedCollateralA[user] = lockedCollateralA[user].add(amount);
+            lockedCollateralA[user] = lockedCollateralA[user] + amount;
         } else if (tokenAddress == address(tokenB)) {
             tokenB.transferFrom(user, address(this), amount);
-            lockedCollateralB[user] = lockedCollateralB[user].add(amount);
+            lockedCollateralB[user] = lockedCollateralB[user] + amount;
         }
     }
     
-    // Function to unlock collateral from the contract
     function unlockCollateral(address user, address tokenAddress, uint256 amount) external {
         require(amount > 0, "Amount must be greater than 0");
         require(tokenAddress == address(tokenA) || tokenAddress == address(tokenB), "Invalid token address");
         
         if (tokenAddress == address(tokenA)) {
             require(lockedCollateralA[user] >= amount, "Insufficient collateral");
-            lockedCollateralA[user] = lockedCollateralA[user].sub(amount);
+            lockedCollateralA[user] = lockedCollateralA[user] - amount;
             tokenA.transfer(user, amount);
         } else if (tokenAddress == address(tokenB)) {
             require(lockedCollateralB[user] >= amount, "Insufficient collateral");
-            lockedCollateralB[user] = lockedCollateralB[user].sub(amount);
+            lockedCollateralB[user] = lockedCollateralB[user] - amount;
             tokenB.transfer(user, amount);
         }
     }
